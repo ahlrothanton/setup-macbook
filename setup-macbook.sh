2300f63ba21b0e98c9d5ff1c344d986a726fabd9
Anton Ahlroth <ahlrothanton@gmail.com>
2021-02-24 10:37:19 +0200

    Simplifies the installation script.

--- setup-macbook.sh
+++ setup-macbook.sh
@@ -4,74 +4,41 @@
 #   Simple script to setup Macbook for my liking.
 #
 # TODO:
-#   - don't download anything, when there is no need
 #   - no user input required
-#   - setup zsh(theme, aliases, etc.) automatically
+#   - setup zsh(theme, aliases, etc.) automatically - use Ansible?
 
-
-# setup variables
-TF_VERSION="0.14.7"
 
 clear
-printf ' --- starting setup ---\n   ! if any step fails, please verify System Preferences > Security & Privacy > General\n\n'
+printf "\n--- starting setup ---\n  ! if any step fails, please verify System Preferences > Security & Privacy > General\n"
+sleep 3
 
 # install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+printf "\n--- downloading and installing Homebrew ---\n"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # update and upgrade Homebrew
+printf "\n--- update and upgrade Homebrew ---\n"
 brew update
 brew upgrade
 
-# install git
-brew install git
+# install brews
+for BREW in git golang ansible terraform; do
+    printf "\n--- installing ${BREW} ---\n"
+    brew install "${BREW}"
+done
 
-# install Atom
-brew install --cask atom
-
-# install iterm2
-brew install --cask iterm2
+# install casks
+for CASK in atom iterm2 docker virtualbox vagrant vagrant-manager slack opera caffeine spotify; do
+    printf "\n--- installing ${CASK} ---\n"
+    brew install --cask "${CASK}"
+done
 
 # install oh-my-zsh
-$ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# install Go
-brew install golang
-
-# install Docker
-brew install --cask docker
-
-# install VirtualBox
-brew install --cask virtualbox
-
-# install Vagrant
-brew install --cask vagrant
-
-# install Vagrant Manager
-brew install --cask vagrant-manager
-
-# install Ansible
-brew install ansible
-
-# install Terraform
-curl -s -k -L -O https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_darwin_amd64.zip
-unzip terraform_${TF_VERSION}_darwin_amd64.zip
-rm -rf terraform_${TF_VERSION}_darwin_amd64.zip
-sudo mv terraform /usr/local/bin/
-
-# install Slack
-brew install --cask slack
-
-# install Opera
-brew install --cask opera
-
-# install caffeine
-brew install --cask caffeine
-
-# install Spotify
-brew install --cask spotify
+printf "\n--- downloading and installing oh-my-zsh ---\n"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # wait 10 seconds
-printf '\n --- waiting for 10 seconds before rebooting the system --- \n'
+printf "\n--- waiting for 10 seconds before rebooting the system --- \n"
 sleep 10
 
 # reboot the system
