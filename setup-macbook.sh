30af3d474b58ee93cf53b6e12a389e47371baa1f
Anton Ahlroth <ahlrothanton@gmail.com>
2021-03-11 11:39:12 +0200

    Revert "Adds aws-iam-authenticator. Adds a to reboot."
    This reverts commit 09118b32736f390e2bee826a5ea9fd44850bbea8.

--- setup-macbook.sh
+++ setup-macbook.sh
@@ -3,30 +3,15 @@
 # Description:
 #   Simple script to setup Macbook for my liking.
 #
-# Usage:
-#   install tools:              ./setup-macbook.sh
-#   install tools and reboot    ./setup-macbook.sh -r
-#
 # TODO:
 #   - no user input required
 #     - at least brew installation requires sudo password and confirmation
 #   - setup zsh(theme, aliases, etc.) automatically - use Ansible?
-#   - only install environment(eg. aws, gcp, all, etc.) specific tools, for example: use cli flag -e aws
 
 
 clear
 printf "\n--- starting setup ---\n  ! if any step fails, please verify System Preferences > Security & Privacy > General\n"
 sleep 3
-
-# parse CLI flags
-while getopts "r" opts; do
-    case "${opts}" in
-        r)
-            BOOT=true
-            ;;
-    esac
-done
-
 
 # install Homebrew
 printf "\n--- downloading and installing Homebrew ---\n"
@@ -46,7 +31,7 @@ printf "\n--- upgrading Homebrew packages ---\n"
 brew upgrade
 
 # install brews
-for BREW in git vim watch jq golang kubectl helm ansible terraform packer awscli aws-cdk eksctl aws-iam-authenticator; do
+for BREW in git vim golang kubectl helm ansible terraform packer awscli aws-cdk watch eksctl; do
     printf "\n--- installing ${BREW} ---\n"
     brew install "${BREW}"
 done
@@ -61,13 +46,9 @@ done
 printf "\n--- downloading and installing oh-my-zsh ---\n"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# wait 10 seconds
+printf "\n--- waiting for 10 seconds before rebooting the system --- \n"
+sleep 10
 
-# reboot the system, if -r flag was given
-if [ "$BOOT" = true ] ; then
-    # wait 10 seconds
-    printf "\n--- waiting for 10 seconds before rebooting the system --- \n"
-    sleep 10
-
-    # reboot
-    sudo reboot
-fi
+# reboot the system
+sudo reboot
